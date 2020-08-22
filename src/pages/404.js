da20b2d8eed80d112ca09a9f07f532102cb620fc
VedatOlmez <vedatcenin@gmail.com>
2020-08-22 12:50:39 +0300

    Initial Commit

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '@common/Layout';
+import { Container } from '@components/global';
+
+const NotFoundPage = () => (
+  <Layout>
+    <Container>
+      <h1>ARADIĞINIZ SAYFA BULUNAMADI</h1>
+      <p>Geldiğin yoldan geri dön!</p>
+    </Container>
+  </Layout>
+);
+
+export default NotFoundPage;
